39fdb17ec670d36f1266d563aeab6900b0e193f5
avideb24 <debavi24@gmail.com>
2025-08-19 19:28:39 +0600

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,6 @@ const App = () => {
 
   return (
     <div className='w-screen h-screen flex justify-center items-center bg-white'>
-      {/* update */}
       <div>
         {
           !isComplete && (
