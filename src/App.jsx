a8b9d41d22565e34c7dd344c9dd3242d165a06cb
Avi Debnath <devavi24bd@gmail.com>
2025-09-14 21:30:19 +0600

    updted new

--- src/App.jsx
+++ src/App.jsx
@@ -22,16 +22,16 @@ const App = () => {
   return (
     <div className="w-screen h-screen flex justify-center items-center bg-white">
       <div>
+        {/* udpated */}
         {!isComplete && (
           <div className="flex flex-col items-center gap-10">
             <h2 className="text-pink-600 text-xl font-bold">Touch & Hold</h2>
             <IoFingerPrintSharp
               className={`text-8xl cursor-pointer transition-all duration-300 
-                  ${
-                    isPressed
-                      ? "text-pink-500 drop-shadow-lg scale-110"
-                      : "text-gray-800 hover:text-pink-600"
-                  }
+                  ${isPressed
+                  ? "text-pink-500 drop-shadow-lg scale-110"
+                  : "text-gray-800 hover:text-pink-600"
+                }
                 `}
               onMouseDown={handlePressStart}
               onMouseUp={handlePressEnd}
